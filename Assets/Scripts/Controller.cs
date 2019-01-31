e9a55f07b6f1ead08908644751f770e22255c3b6
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-01-31 17:08:02 -0600

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private float speedwagon;
+    private bool start;
+    private int points;
+    public TextMesh textPoint;
+
+    void GameOver()
+    {
+        start = false;
+        transform.position = new Vector3(-0.2f, 0.82f, -5179f);
+    }
+
+    void Win()
+    {
+
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedwagon = 8f;
+        start = false;
+        points = 70;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+             start = true;
+        }
+        if (start) { 
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speedwagon, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speedwagon);
+            textPoint.text = "Points:" + points.ToString();
+        }
+    }
+    private void OncollisionEnter(Collision collision)
+    {
+    
+        if(collision.collider.tag == "cubes")
+        {
+            points = points - 10;
+            Destroy(collision.gameObject);
+            
+        }else if(collision.collider.name == "Wall3")
+        {
+            Win();
+            GameOver();
+        }
+    }
+}
